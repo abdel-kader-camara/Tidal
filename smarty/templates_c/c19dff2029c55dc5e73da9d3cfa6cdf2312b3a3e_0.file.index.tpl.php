21581bdccf29cfa2cdb9a8aaa83d481aa1a6c9e7
abdel-kader-camara <71006193+abdel-kader-camara@users.noreply.github.com>
2021-09-13 17:02:45 +0200

    temp

---
+++ smarty/templates_c/c19dff2029c55dc5e73da9d3cfa6cdf2312b3a3e_0.file.index.tpl.php
@@ -0,0 +1,34 @@
+<?php
+/* Smarty version 3.1.39, created on 2021-09-13 16:55:11
+  from '/var/www/html/Tidal/smarty/templates/index.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_613f664f13e397_77071929',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    'c19dff2029c55dc5e73da9d3cfa6cdf2312b3a3e' => 
+    array (
+      0 => '/var/www/html/Tidal/smarty/templates/index.tpl',
+      1 => 1631543816,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_613f664f13e397_77071929 (Smarty_Internal_Template $_smarty_tpl) {
+?><html>
+  <head>
+    <title>Smarty</title>
+  </head>
+  <body>
+    Hello, <?php echo $_smarty_tpl->tpl_vars['name']->value;?>
+!
+  </body>
+</html>
+<?php }
+}

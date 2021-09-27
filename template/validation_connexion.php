334b512ebd5ac23cb3f7cd6d24a6e22b45b9c8ec
Rémi GRUFFAT <remi.gruffat@cpe.fr>
2021-09-27 16:12:21 +0200

    Validation PHP

--- template/validation_connexion.php
+++ template/validation_connexion.php
@@ -1,44 +1,37 @@
 <?php
+$serveur = "localhost";
+$dbname = "acu";
+$user = "tidal";
+$pass = "tidal";
+
+
+$nom = $_POST["nom"];
+$prenom = $_POST["prenom"];
+$email = $_POST["email"];
+$mdp = $_POST["mdp"];
 
 if ($_POST){
 $form = $_POST;
 $login = $form['login'];
 $password = $form['password'];
+$hash_pwd = password_hash($password, PASSWORD_DEFAULT);
 
 try{
-    $db = new PDO('mysql:host=localhost;dbname=phpproject', 'root', 'pdt1848!');
+    $db = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
-    catch(PODException $e){
+    catch(PDOException $e){
         echo "Can't connect to the database";
     }
-$sql = "SELECT * FROM users WHERE username=:username";
+$sql = "SELECT mdp FROM inscription WHERE nom=:nom";
 $query = $db->prepare($sql);
-$query->execute(array(':username'=>$username, ':password'=>$stored_hash));
-$results = $query->fetchAll(PDO::FETCH_ASSOC);
-
-
-$check = $hash_obj->CheckPassword($password, $stored_hash);
-if($check){
-    print_r("Registered user");
-}
-else{
-    print_r("Not a registered user");
-}
+$query->execute(array(':nom'=>$login));
+$results = $query->fetchAll();
+print_r($results);
+if ($results['mdp'] == $hash_pwd )  echo "Password OK ! "; 
+else echo "password NOK ! ";
 
 
 //login here
 } 
-else{
-?>
-<form name="login" action="login.php" method="POST">
-<label for "username">Username: </label>
-<input type="text" name="username"/><br />
-<label for "password">Password: </label>
-<input type="password" name="password"/><br />
-<button type="submit">Submit</button>
-<button type="reset">Reset Form</button>
-</form>
-<?php
-}
 ?>

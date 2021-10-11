2da6f7ec4573f62f53d5256c5fea1fae256b42c4
Rémi GRUFFAT <remi.gruffat@cpe.fr>
2021-10-11 14:21:27 +0200

    test temp

--- template/listesymptome.php
+++ template/listesymptome.php
@@ -1,16 +1,6 @@
 <?php
-$serveur = "localhost";
-$dbname = "acu";
-$user = "tidal";
-$pass = "tidal";
+require_once('template_bdd.php');
 //create connection
-try{
-    $db = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-    catch(PDOException $e){
-        echo "Can't connect to the database";
-    }
 $sql = "SELECT * FROM symptome";
 $query = $db->prepare($sql);
 $query->execute();
--- template/template_bdd.php
+++ template/template_bdd.php
@@ -3,4 +3,11 @@ $serveur = "localhost";
 $dbname = "acu";
 $user = "tidal";
 $pass = "tidal";
+try{
+    $db = new PDO("mysql:host=$serveur;dbname=$dbname",$user,$pass);
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+    catch(PDOException $e){
+        echo "Can't connect to the database";
+    }
 ?>

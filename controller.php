c6709cd0239ff8c895724af7c1789cd4b23d2c2b
Rémi GRUFFAT <remi.gruffat@cpe.fr>
2021-10-11 16:42:53 +0200

    pages

--- controller.php
+++ controller.php
@@ -3,7 +3,7 @@ include("template/template_bdd.php");
 
 // put full path to Smarty.class.php
 require('/usr/local/lib/php/Smarty/Smarty.class.php');
-
+$smarty = new Smarty();
 $smarty->setTemplateDir('/var/www/html/projet/Tidal/Tidal/smarty/templates');
 $smarty->setCompileDir('/var/www/html/projet/Tidal/Tidal/smarty/templates_c');
 $smarty->setCacheDir('/var/www/html/projet/Tidal/Tidal/smarty/cache');

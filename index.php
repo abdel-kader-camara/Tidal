c995ca9818d9dcc4b63a2acf8c8bb5cd6208197d
Rémi GRUFFAT <remi.gruffat@cpe.fr>
2021-11-08 15:05:10 +0100

    tata

--- index.php
+++ index.php
@@ -1,28 +1,23 @@
 <?php
 require_once("controller.php");
+$pages = $_GET['pages'];
 session_start();
 if (isset($_SESSION['login']) && isset($_SESSION['pwd'])) {
 
     echo $_SESSION['login'];
     echo $_SESSION['pwd'];
+}
 
 
- }
-
-
- else 
-
-
-$pages = $_GET['pages'];
-if($pages == "accueil" ) {
+elseif($pages == "accueil" ) {
     accueil();
 }
-if($pages == "inscription" ) {
+elseif($pages == "inscription" ) {
     
     inscription();
 }
 
-if($pages == "connexion" ) {
+elseif($pages == "connexion" ) {
     
     connexion();
 }

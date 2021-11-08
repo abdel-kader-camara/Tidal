9e0f8903f557e715d71d98db4beb927957add4e4
abdel-kader-camara <abdel-kader.camara@cpe.fr>
2021-11-08 16:19:27 +0100

    bon

--- templates_c/0197cd10839899a77adc83e8cbd418f0f690a696_0.file.index.tpl.php
+++ templates_c/0197cd10839899a77adc83e8cbd418f0f690a696_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-11-08 16:11:07
+/* Smarty version 3.1.39, created on 2021-11-08 16:18:13
   from '/var/www/html/projet/Tidal/Tidal/smarty/templates/index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_61893e0ba5d301_24075919',
+  'unifunc' => 'content_61893fb5b859f3_14204035',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '0197cd10839899a77adc83e8cbd418f0f690a696' => 
     array (
       0 => '/var/www/html/projet/Tidal/Tidal/smarty/templates/index.tpl',
-      1 => 1636384258,
+      1 => 1636384689,
       2 => 'file',
     ),
   ),
@@ -22,16 +22,18 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:./footer.tpl' => 1,
   ),
 ),false)) {
-function content_61893e0ba5d301_24075919 (Smarty_Internal_Template $_smarty_tpl) {
+function content_61893fb5b859f3_14204035 (Smarty_Internal_Template $_smarty_tpl) {
 ?> 
 <?php $_smarty_tpl->_subTemplateRender("file:./header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
 ?>
 
-if (isset (<?php echo $_SESSION['login'];?>
-)) {
-  <p>tiamat</p>
-}
+<?php ob_start();
+echo $_SESSION['login'];
+$_prefixVariable1 = ob_get_clean();
+if ((isset($_prefixVariable1))) {?>
+  Bienvenue <?php echo $_SESSION['login'];?>
 
+<?php }?>
  <div class="header">
     <button class="button" onclick="window.location.href = '../Tidal/index.php?pages=connexion';">Connexion</button>
     <button class="button" onclick="window.location.href = '../Tidal/index.php?pages=inscription';">Inscription</button>

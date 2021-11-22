6a4d758f975410005a89e0a8598de7d644bd00fd
abdel-kader-camara <abdel-kader.camara@cpe.fr>
2021-11-22 16:00:04 +0100

    tidal

--- templates_c/0197cd10839899a77adc83e8cbd418f0f690a696_0.file.index.tpl.php
+++ templates_c/0197cd10839899a77adc83e8cbd418f0f690a696_0.file.index.tpl.php
@@ -1,18 +1,18 @@
 <?php
-/* Smarty version 3.1.39, created on 2021-11-08 16:49:37
+/* Smarty version 3.1.39, created on 2021-11-22 15:41:47
   from '/var/www/html/projet/Tidal/Tidal/smarty/templates/index.tpl' */
 
 /* @var Smarty_Internal_Template $_smarty_tpl */
 if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
   'version' => '3.1.39',
-  'unifunc' => 'content_61894711639349_12478594',
+  'unifunc' => 'content_619bac2b3e5d34_45130330',
   'has_nocache_code' => false,
   'file_dependency' => 
   array (
     '0197cd10839899a77adc83e8cbd418f0f690a696' => 
     array (
       0 => '/var/www/html/projet/Tidal/Tidal/smarty/templates/index.tpl',
-      1 => 1636386504,
+      1 => 1637592104,
       2 => 'file',
     ),
   ),
@@ -22,10 +22,12 @@ if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
     'file:./footer.tpl' => 2,
   ),
 ),false)) {
-function content_61894711639349_12478594 (Smarty_Internal_Template $_smarty_tpl) {
+function content_619bac2b3e5d34_45130330 (Smarty_Internal_Template $_smarty_tpl) {
 ?> 
 <?php $_smarty_tpl->_subTemplateRender("file:./header.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, 0, $_smarty_tpl->cache_lifetime, array(), 0, false);
-if ((isset($_SESSION['login']))) {?>
+?>
+
+<?php if ((isset($_SESSION['login']))) {?>
   Bienvenue <?php echo $_SESSION['login'];?>
 
 
@@ -34,9 +36,9 @@ if ((isset($_SESSION['login']))) {?>
     <div class="col-10 col-s-10 menu">
      
       <ul>
-        <li>Accueil</li>
-        <li>Symptomes</li>
-        <li>Pathologie</li>
+        <li><a href="../Tidal/index.php?pages=accueil">Accueil</a></li>
+        <li><a href="../Tidal/template/listesymptome.php">Symptomes</a></li>
+        <li><a href="../Tidal/template/filtre.php">Pathologie</a></li>
       </ul>
     </div>
   </div>

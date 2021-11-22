feae43134fbe25bb0f175c97039c4502a433b616
Rémi GRUFFAT <remi.gruffat@cpe.fr>
2021-11-22 16:26:48 +0100

    api

---
+++ template/api.php
@@ -0,0 +1,22 @@
+<?php
+require_once('template_bdd.php');
+
+if (isset ($_GET['filtre'])) {
+$filtre = $_GET['filtre'];
+$sql = 'SELECT S.desc AS symptome, P.desc AS pathologie FROM `keywords` AS KW JOIN keySympt AS KS on KW.idK = KS.idK JOIN symptome AS S on KS.idS = S.idS JOIN symptPatho AS SP on S.idS = SP.idS JOIN patho AS P on SP.idP = P.idP WHERE KW.name LIKE :filtre ';
+$query = $db->prepare($sql);
+$query->execute(array(':filtre'=>$filtre));
+$results = $query->fetch(PDO::FETCH_ASSOC);
+echo json_encode($results,JSON_PRETTY_PRINT);
+
+}
+
+else { 
+$sql = "SELECT * FROM symptome";
+$query = $db->prepare($sql);
+$query->execute();
+$results = $query->fetch(PDO::FETCH_ASSOC);
+echo json_encode($results,JSON_PRETTY_PRINT);
+}
+
+?>

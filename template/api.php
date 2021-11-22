1a45214bd8104e93c29735a6606145c749c108f2
Rémi GRUFFAT <remi.gruffat@cpe.fr>
2021-11-22 16:40:52 +0100

    ez

--- template/api.php
+++ template/api.php
@@ -6,21 +6,20 @@ $filtre = $_GET['filtre'];
 $sql = 'SELECT S.desc AS symptome, P.desc AS pathologie FROM `keywords` AS KW JOIN keySympt AS KS on KW.idK = KS.idK JOIN symptome AS S on KS.idS = S.idS JOIN symptPatho AS SP on S.idS = SP.idS JOIN patho AS P on SP.idP = P.idP WHERE KW.name LIKE :filtre ';
 $query = $db->prepare($sql);
 $query->execute(array(':filtre'=>$filtre));
-$results = $query->fetch(PDO::FETCH_ASSOC);
-foreach ($results as $field => $value){
-    echo json_encode($field,JSON_PRETTY_PRINT);
+$results = $query->fetchAll();
 
-}
+echo json_encode($field,JSON_PRETTY_PRINT);
+
+
 }
 
 else { 
 $sql = "SELECT * FROM symptome";
 $query = $db->prepare($sql);
 $query->execute();
-$results = $query->fetch(PDO::FETCH_ASSOC);
-for ($i = 1; $i <= 10; $i++) {
-    echo json_encode($results,JSON_PRETTY_PRINT);
-}
+$results = $query->fetchAll();
+
+echo json_encode($field,JSON_PRETTY_PRINT);
 }
 
 ?>

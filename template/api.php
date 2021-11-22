8717c6ba48d731a45c7abc9029b9c16484fe9254
Rémi GRUFFAT <remi.gruffat@cpe.fr>
2021-11-22 16:46:39 +0100

    api

--- template/api.php
+++ template/api.php
@@ -6,6 +6,7 @@ $filtre = $_GET['filtre'];
 $sql = 'SELECT S.desc AS symptome, P.desc AS pathologie FROM `keywords` AS KW JOIN keySympt AS KS on KW.idK = KS.idK JOIN symptome AS S on KS.idS = S.idS JOIN symptPatho AS SP on S.idS = SP.idS JOIN patho AS P on SP.idP = P.idP WHERE KW.name LIKE :filtre ';
 $query = $db->prepare($sql);
 $query->execute(array(':filtre'=>$filtre));
+header("Content-Type: JSON");
 $results = $query->fetchAll();
 $field["Message"] = "Liste des symptomes : ";
 $field["Resulats"] = $results;
@@ -19,6 +20,7 @@ else {
 $sql = "SELECT * FROM symptome";
 $query = $db->prepare($sql);
 $query->execute();
+header("Content-Type: JSON");
 $results = $query->fetchAll();
 
 $field["Message"] = "Liste des symptomes : ";
